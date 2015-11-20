04497c6acc169829d4eb98ddf654ba2dd7a347bc
s.murakami <smurakami.84@gmail.com>
2015-11-20 16:18:10 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from figure import Figure
+import cv2
+
+class Main:
+    def __init__(self):
+        img = cv2.imread('okamura.png')
+        figure = Figure(img)
+        while True:
+            figure.draw()
+            key = cv2.waitKey(33)
+            if key == ord('q'):
+                break
+
+
+
+if __name__ == '__main__':
+    Main()
+
